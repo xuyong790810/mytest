ad89e6fc8f5cbb2135d841846a7b34afad9d67bc
徐勇 <xuyong@xuyongdemacbook-pro.local>
2018-11-22 15:43:05 +0800

    mytest

--- gittest/gittest/ViewController.swift
+++ gittest/gittest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("111")
+        print("33333")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
